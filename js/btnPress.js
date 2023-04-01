e0abf1eb6e51a17f24462882e16dc3f597b5796b
estevamwellerson <71524215+estevamwellerson@users.noreply.github.com>
2023-04-01 15:28:06 -0300

    Projeto-Calculadora-js-ESmodules

---
+++ js/btnPress.js
@@ -0,0 +1,50 @@
+import { calculate } from "./calculate.js";
+const input = document.querySelector("#input")
+
+function buttonPress (ev) {
+    const value = ev.currentTarget.dataset.value;
+    input.value += value;
+  };
+
+  
+function buttonClear() {
+  input.value = "";
+  input.focus();
+};
+
+function typing (ev) {
+  const allowedKeys = [
+    "(",
+    ")",
+    "/",
+    "*",
+    "-",
+    "+",
+    "9",
+    "8",
+    "7",
+    "6",
+    "5",
+    "4",
+    "3",
+    "2",
+    "1",
+    "0",
+    ".",
+    "%",
+    " ",
+  ];
+  ev.preventDefault();
+  if (allowedKeys.includes(ev.key)) {
+    input.value += ev.key;
+    return;
+  }
+  if (ev.key === "Backspace") {
+    input.value = input.value.slice(0, -1);
+  }
+  if (ev.key === "Enter") {
+    calculate();
+  }
+}
+
+export { buttonPress, buttonClear, typing };
